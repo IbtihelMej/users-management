f79dbba7f0c834d38b547fb4191f23fc7c4e980a
ibtihelMejri <ibtihel.mejri@adn-expertise.fr>
2022-07-20 14:17:34 +0100

    #P007 create interface sign in

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import "./App.css";
 import SignIn from "./Components/Signin";
 import Users from "./Components/Users";
 import { Route, Routes } from "react-router-dom";
+// -- Component Styles
+// import "./Styles/app.scss";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">  
-        </header> */}
-        <Routes>
-        <Route
-        path="/signin"
-        element={ <SignIn />}
-      />
-        <Route
-        path="/users"
-        element={ <Users />}
-      />
-        </Routes>
+
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Signin/index.js
+++ src/Components/Signin/index.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import {
+  Avatar,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="signin">
+      <Grid>
+        <Paper elevation={10} className="paper" >
+          <Grid align="center">
+            <Avatar style={{ backgroundColor: "#38b474" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Se connecter</h2>
+          </Grid>
+          <TextField
+            style={{ margin: "1em auto 2em" }}
+            required
+            id="email"
+            label="Email"
+            variant="outlined"
+            placeholder="Entrer votre email"
+            fullWidth
+          />
+          <TextField
+            required
+            type="password"
+            id="password"
+            label="Mot de passe"
+            variant="outlined"
+            placeholder="Entrer votre mot de passe"
+            fullWidth
+          />
+
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Se souvenir de moi"
+          />
+
+          <Button
+            style={{ margin: "20px 0" }}
+            type="submit"
+            color="primary"
+            fullWidth
+            variant="contained"
+          >
+            Se connecter
+          </Button>
+
+          <Typography style={{marginBottom: "1em"}}>
+            <Link href="#">Mot de passe oublié?</Link>
+          </Typography>
+
+          <Typography>
+            {" "}
+            Avez-vous un compte ? <Link href="#">S'inscrire</Link>
+          </Typography>
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
 
 export default SignIn;
